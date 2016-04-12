4eebca88d568b30ff710836a8b30418adac519ea
ssmandyam <sri_mandyam@yahoo.com>
2016-04-12 13:25:54 -0700

    updating code

--- run_analysis.R
+++ run_analysis.R
@@ -11,26 +11,24 @@ sub_testdf <- read.table("./test/subject_test.txt", stringsAsFactors=FALSE)
 act_label <- read.table("./activity_labels.txt", stringsAsFactors=FALSE)
 feat_df <- read.table("./features.txt", stringsAsFactors=FALSE)
 
-# Step 2: bind test and train sets
+# Step 2: bind test and train sets and assign column names
 M <- rbind(x_traindf, x_testdf)
-
-#Step 3: Extracting the mean and standard deviation
-measurement <- apply(M,2, function(x) c(m=mean(x), s=sd(x)))
-
-#Step 4: fix duplicates and assign labels
-for (i in which(duplicated(feat_df$V2))) {
-	feat_df$V2[i] = paste(feat_df$V2[i], "_dupe", as.character(i),sep="")
-}
 colnames(M) <- feat_df$V2
-
-#Step 5: bind subject and activity columns to test and train sets
 M$activity <- rbind(y_traindf, y_testdf)$V1
-M$activity <- sapply(M$activity, function(x) act_label$V2[x])
 M$subject <- rbind(sub_traindf, sub_testdf)$V1
 
-#Step 6: create a tidy data set with average of each variable for subject and activity
+# assign variable values for activities
+M$activity <- sapply(M$activity, function(x) act_label$V2[x])
+
+#Step 3: Extracting the mean and standard deviation columns only
+library(dplyr)
+Mtbl <- tbl_df(M)
+mean_std_vars <- c(grep("mean()",names(M), value=TRUE), grep("std()", names(M), value=TRUE))
+Mtbl <- select(Mtbl, one_of("subject", "activity", mean_std_vars))
+
+#Step 4: create a tidy data set with average of each variable for subject and activity
 library(reshape2)
-tmelt <- melt(M, id.vars=c("subject", "activity"), measure.vars=feat_df$V2)
+tmelt <- melt(Mtbl, id.vars=c("subject", "activity"), measure.vars=mean_std_vars)
 tcast <- dcast(tmelt, subject+activity~variable, mean)
 write.table(tcast, "tidy_data.txt", row.name=FALSE)
 
